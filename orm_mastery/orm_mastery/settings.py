a73f37120bf3a0668b8615d8bf01327560d5a55f
Deku <yadavadarshgamer@gmail.com>
2024-07-01 19:26:53 +0530

    fourth commit

--- orm_mastery/orm_mastery/settings.py
+++ orm_mastery/orm_mastery/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "testapp",
     "django_extensions",
 ]
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -123,3 +125,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
--- orm_mastery/testapp/views.py
+++ orm_mastery/testapp/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
-
+from .models import Restaurant
 # Create your views here.
 from .forms import RatingForm
 
 
 def index(request):
-    if request.method == "POST":
-        form = RatingForm(request.POST or None)
-        if form.is_valid():
-            form.save()
+    # if request.method == "POST":
+    #     form = RatingForm(request.POST or None)
+    #     if form.is_valid():
+    #         form.save()
 
-        else:
-            return render(request, "index.html", {"form": form})
-    context = {"form": RatingForm()}
+    #     else:
+    #         return render(request, "index.html", {"form": form})
+    # context = {"form": RatingForm()}
+    restaurants=Restaurant.objects.all()
+    context={'restaurants':restaurants}
 
     return render(request, "index.html", context)

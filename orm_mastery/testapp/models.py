3a57aba8b49fae815b71e27d66bfb738baf4e720
Deku <yadavadarshgamer@gmail.com>
2024-07-09 17:54:28 +0530

    c1

--- orm_mastery/testapp/models.py
+++ orm_mastery/testapp/models.py
@@ -57,6 +57,6 @@ class Rating(models.Model):
 
 
 class Sale(models.Model):
-    restaurant=models.ForeignKey(Restaurant,on_delete=models.SET_NULL,null=True)
+    restaurant=models.ForeignKey(Restaurant,on_delete=models.SET_NULL,null=True,related_name='sales')
     income=models.DecimalField(max_digits=8,decimal_places=2)
     datetime=models.DateField()
--- orm_mastery/testapp/views.py
+++ orm_mastery/testapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from .models import Restaurant
+from .models import Restaurant, Rating,Sale
+from django.db.models import Prefetch,Sum
+from django.utils import timezone
+
 # Create your views here.
 from .forms import RatingForm
 
@@ -13,7 +16,15 @@ def index(request):
     #     else:
     #         return render(request, "index.html", {"form": form})
     # context = {"form": RatingForm()}
-    restaurants=Restaurant.objects.all()
-    context={'restaurants':restaurants}
+    # restaurants=Restaurant.objects.filter(name__istartswith='c').prefetch_related('ratings','sales')
+    # context={'restaurants':restaurants}
+    month_ago=timezone.now()-timezone.timedelta(days=30)
+    monthly_sales=Prefetch('sales',queryset=Sale.objects.filter(datetime__gte=month_ago))
+    # ratings = Rating.objects.only('rating','restaurant__name').select_related("restaurant")
+    restaurants=Restaurant.objects.prefetch_related('ratings',monthly_sales).filter(ratings__rating=5)
+    restaurants=restaurants.annotate(total=Sum('sales__income'))
 
+    context = {"restaurants": restaurants}
+
+    print([r.total for r in restaurants])
     return render(request, "index.html", context)
